9a8b0e1dff63f462324039adda3b4d84f340c424
Cathy Ji <jicathyh@gmail.com>
2013-04-26 00:12:29 -0700

    Added implementation for laser 8

--- laser.cpp
+++ laser.cpp
@@ -1,8 +1,13 @@
 #include "laser.h"
 
+Laser::Laser(QPixmap* pic, int w, int h, int vx, int vy, Rocket* rocket ) : Thing(pic, rocket->getWidth()/2, rocket->getY(), w, h, vx, vy, 1 ){
+
+  rocket_ = rocket;
+}
+
+
 Laser::Laser(){
 }
 
 Laser::~Laser(){
 }
-
--- laser.h
+++ laser.h
@@ -1,16 +1,19 @@
 #ifndef LASER_H
 #define LASER_H
 
-#include "thing.h" 
+#include "thing.h"
+#include "rocket.h"
 
 /* Laser moves in direction from spaceship to mouse. Starting position is tip of the rocketship*/
 
 class Laser : public Thing {
   public:
+    Laser(QPixmap* pic, int w, int h, int vx, int vy, Rocket* rocket);
     Laser();
     ~Laser();
     void move(int x, int y);
     
   private:
+    Rocket* rocket_;
 };
 #endif //LASER_H
