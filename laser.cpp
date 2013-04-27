117b817b198a121c6f675aeef15905d4bc5a6c83
Cathy Ji <jicathyh@gmail.com>
2013-04-26 22:46:46 -0700

    Fixed laser movement for real close #8

--- laser.cpp
+++ laser.cpp
@@ -1,6 +1,6 @@
 #include "laser.h"
 using namespace std;
-Laser::Laser(QPixmap* pic, int vx, int vy, Rocket* rocket ) : Thing(pic, rocket->getWidth()/2, rocket->getY(), vx, vy, 1 ){
+Laser::Laser(QPixmap* pic, int vx, int vy, Rocket* rocket ) : Thing(pic, rocket->getWidth()/2+rocket->getX(), rocket->getY(), vx, vy, 1 ){
 
   rocket_ = rocket;
 }
@@ -13,24 +13,16 @@ Laser::~Laser(){
 }
 
 void Laser::findStartingPoint(){
-  setX( rocket_->getWidth()/2 + rocket_->getX());
-  setY( rocket_->getY() - rocket_->getHeight()+height_);
 }
 
 void Laser::shoot(int x, int y){
   findStartingPoint();
-  int xdir = 1, ydir = 1;
-
   int xvel = 0, yvel = 0;
   xvel = (x - x_) / velocityX_;
   yvel = (y - y_) / velocityY_;
   
-  cout << xvel << " " <<yvel<<endl;
-  
-  setVx( xdir * xvel );
-  setVy( ydir * yvel );
-  
-  cout << velocityX_ << " " << velocityY_ <<endl;
+  setVx( xvel );
+  setVy( yvel );
   
   
   
