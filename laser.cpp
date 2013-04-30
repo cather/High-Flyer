23f7ef40ba568217cebff48cd7c4b933e66f0032
Cathy Ji <jicathyh@gmail.com>
2013-04-29 17:30:12 -0700

    added doxygen comment and amended collide function to hurt enemy and make laser self destruct

--- laser.cpp
+++ laser.cpp
@@ -9,7 +9,7 @@ using namespace std;
 Laser::Laser(QPixmap* pic, int w, int h, int vx, int vy, Rocket* rocket) :
   Thing(pic, rocket->getX()+(rocket->getWidth()/2), rocket->getY()-20, w, h, vx, vy, 1 ){
   offScreen = false;
-  dead=false;
+  dead = false;
 }
 
 /** Constructor */
@@ -20,15 +20,16 @@ Laser::Laser(){
 Laser::~Laser(){
 }
 
-/** Moves laser*/
+/** Sets velocity equal to a number corresponding to the mouse's position on the screen
+  @param mousePointX the x coord of the mouseclick
+  @param mousePointY the y coord of the mouseclick
+*/
 void Laser::shoot(int mousePointX, int mousePointY){
   int xvel = (mousePointX - x_)/velocityX_;
   int yvel = (mousePointY-y_) / velocityY_;
-  cout <<xvel << " "<<yvel<<endl;
   setVx( xvel );
   setVy( yvel );
 }
-
 
 /** If colliding with a Thing, decrements that Thing's health by 1, kills itself and returns true. Returns false otherwise.
   @param enemy the Thing the Laser checks if it's intersecting
@@ -36,6 +37,9 @@ void Laser::shoot(int mousePointX, int mousePointY){
 bool Laser::collidesWith(Thing* enemy){
   bool collide = collidesWithItem(enemy, Qt::IntersectsItemShape);
   if (collide == true)
-    cout <<"collides"<<endl;
+  {
+    enemy->decrementHealth(1); // hurts enemy
+    die(); // kills self
+  }
   return collide;
 }
